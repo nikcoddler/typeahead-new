f1381362dd5bf79bce1feecf752bdc982fa5d2e5
nikki solanki <nikkisolanki10.9@gmail.com>
2025-01-23 10:09:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { SearchBox } from './components/searchBox';
+
+function App() {
+  return (
+    <div className="wrapper">
+     <SearchBox />
+    </div>
+  );
+}
+
+export default App;
